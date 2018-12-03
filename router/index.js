28c1cb0c82e233557349200c25f443d1ec28c449
zhangwenbin <zhangwb@lhcis.com>
2018-12-03 17:06:25 +0800

    'sad'

--- router/index.js
+++ router/index.js
@@ -6,7 +6,6 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/*', async (ctx, next) => {
-  console.log(ctx.request.path)
   let reg = /^\/(api|uploads)/;
   if (!reg.test(ctx.request.path)) {
     ctx.body = 'asda'
@@ -14,17 +13,19 @@ router.get('/*', async (ctx, next) => {
   next();
 });
 router.post('/upload/*', async (ctx, next) => {
-  let file = ctx.request.files.file;
-  let reader = fs.createReadStream(file.path);
+  let file = ctx.request.files;
+  console.log(file,'sdd')
+
+  /*let reader = fs.createReadStream(file.path);
   let date = new Date();
   let newFile = date.getTime() + '.' + file.name.split('.')[1];
   let filePath = path.join(__dirname, '..', 'public/uploads/') + `/${newFile}`;
   let writeFile = fs.createWriteStream(filePath);
-  reader.pipe(writeFile);
+  reader.pipe(writeFile);*/
   ctx.body = {
     code: 200,
     data: {
-      url: 'http://' + ctx.headers.host + '/uploads/' + newFile
+      url: 'http://' + ctx.headers.host + '/uploads/' //+ newFile
     },
     message: 'OK'
   }
