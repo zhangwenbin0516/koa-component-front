99093fd8629e0297a279ca91e94545030c4eea7b
zhangwenbin <zhangwb@lhcis.com>
2018-12-03 10:56:25 +0800

    'static'

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 "use strict"
+const path = require('path');
 const koa = require('koa');
+const koaBody = require('koa-body');
 const router = require('./router/');
+const statics = require('koa-static');
 
 const app = new koa();
 
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    maxFileSize: 500 * 1024 * 1024
+  }
+}));
+
+const fileReady = statics(path.join(__dirname, '/public/'));
+
+app.use(fileReady);
 app.use(router.routes(), router.allowedMethods());
 
 module.exports = app;
--- router/index.js
+++ router/index.js
@@ -1,10 +1,33 @@
 "use strict"
 const fs = require('fs');
+const path = require('path');
 const Router = require('koa-router');
-const router = new Router();
-router.get('/*', async (ctx, next) => {
-    console.log(ctx)
-    ctx.body = 'asda'
-});
 
+const router = new Router();
+
+router.get('/*', async (ctx, next) => {
+  console.log(ctx.request.path)
+  let reg = /^\/(api|uploads)/;
+  if (!reg.test(ctx.request.path)) {
+    ctx.body = 'asda'
+  }
+  next();
+});
+router.post('/upload/*', async (ctx, next) => {
+  let file = ctx.request.files.file;
+  let reader = fs.createReadStream(file.path);
+  let date = new Date();
+  let newFile = date.getTime() + '.' + file.name.split('.')[1];
+  let filePath = path.join(__dirname, '..', 'public/uploads/') + `/${newFile}`;
+  let writeFile = fs.createWriteStream(filePath);
+  reader.pipe(writeFile);
+  ctx.body = {
+    code: 200,
+    data: {
+      url: 'http://' + ctx.headers.host + '/uploads/' + newFile
+    },
+    message: 'OK'
+  }
+  next();
+});
 module.exports = router;
