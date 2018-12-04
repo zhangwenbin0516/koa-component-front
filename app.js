2130eb8d8ff7c25d29f960db2601f10a099984be
zhangwenbin <zhangwb@lhcis.com>
2018-12-04 14:05:05 +0800

    'ss'

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.use(koaBody({
   multipart: true,
   encoding: 'gzip',
   formidable: {
-    uploadDir: path.join(__dirname, './public/uploads'),
     maxFileSize: 500*1024*1024,    // 设置上传文件大小最大限制，默认5M
-    patchKoa: true
   }
 }));
 
